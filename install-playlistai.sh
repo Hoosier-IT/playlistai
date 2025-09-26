a3f3b47e0eea18216000d9ef0e6674bebcd583fc
Hoosier-IT <michael@michaelshirley.us>
2025-09-25 20:58:14 -0400

    Update install-playlistai.sh

--- install-playlistai.sh
+++ install-playlistai.sh
@@ -24,7 +24,7 @@ STORAGE="local-lvm"
 IMAGE="debian-12-standard"
 
 echo "🧠 Creating $APP container (CT $CTID)..."
-pct create $CTID $STORAGE:vztmpl/$IMAGE \
+pct create $CTID local:vztmpl/$IMAGE \
   --hostname playlistai \
   --cores $CORE_COUNT \
   --memory $MEMORY \
