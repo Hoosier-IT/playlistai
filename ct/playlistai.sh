673bae60a30092d963c792590fc1ed6d46e2ca8f
Hoosier-IT <michael@michaelshirley.us>
2025-09-26 21:26:14 -0400

    Update playlistai.sh

--- ct/playlistai.sh
+++ ct/playlistai.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # PlaylistAI LXC Container Script
-# Author: Michael (Hoosier-IT)
+# Author: (Hoosier-IT)
 # License: MIT
 
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
@@ -12,16 +12,18 @@ var_cpu="2"
 var_ram="1024"
 var_disk="4"
 var_unprivileged="1"
-var_storage="local"
 var_tags="music;llm;flask"
 var_hostname="playlistai"
 
+header_info "$APP"
+variables
+color
+catch_errors
+
 function description() {
   echo -e "PlaylistAI: Flask API that curates playlists using Music Assistant and an LLM."
-  echo -e "Binds your host music folder into /data/music inside the container."
 }
 
-# Post-creation hook
 function update_script() {
   header_info
   msg_info "Running PlaylistAI installation inside container $CTID"
@@ -30,5 +32,6 @@ function update_script() {
 }
 
 start
+build_container
 description
 msg_ok "Completed Successfully!"
